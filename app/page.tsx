576f4fde79691fdc07d699a3ce91ebff54ef85e5
Emir Jean Antonios <emir.antonios@gmail.com>
2023-09-14 12:11:58 +0300

    feature: added animation to index

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,47 @@
+"use client";
 import Image from "next/image";
 import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <section className="flex items-center h-full">
       <div className="basis-1/2">
-        <h1 className="text-7xl font-bold pb-8">Emir Jean Antonios</h1>
-        <p className="text-xl leading-8">
-          Merhaba, Ben Emir Jean Antonios. Bilgisayar mühendisi olarak, yazılım
-          dünyasına olan sevgim ve merakım beni sürekli olarak yeni keşiflere ve
-          öğrenmeye yönlendiriyor. Teknolojinin sınırlarını zorlayan bir tutkulu
-          gezgin olarak, deneyimlerimi ve becerilerimi paylaşmak için buradayım.
-        </p>
-        <div className="flex pt-4 gap-4">
-          <SocialIcon
-            style={{ height: 40, width: 40 }}
-            label="github profile"
-            url="https://github.com/EmirJeanAntonios"
-          />
-          <SocialIcon
-            style={{ height: 40, width: 40 }}
-            label="Linkedin profile"
-            url="https://www.linkedin.com/in/emir-jean-antonios-698a82b7/"
-          />
-        </div>
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          className="text-7xl font-bold pb-8"
+        >
+          Emir Jean Antonios
+        </motion.h1>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.4 }}
+        >
+          <p className="text-xl leading-8">
+            Merhaba, Ben Emir Jean Antonios. Bilgisayar mühendisi olarak,
+            yazılım dünyasına olan sevgim ve merakım beni sürekli olarak yeni
+            keşiflere ve öğrenmeye yönlendiriyor. Teknolojinin sınırlarını
+            zorlayan bir tutkulu gezgin olarak, deneyimlerimi ve becerilerimi
+            paylaşmak için buradayım.
+          </p>
+          <div className="flex pt-4 gap-4">
+            <SocialIcon
+              style={{ height: 40, width: 40 }}
+              label="github profile"
+              url="https://github.com/EmirJeanAntonios"
+            />
+            <SocialIcon
+              style={{ height: 40, width: 40 }}
+              label="Linkedin profile"
+              url="https://www.linkedin.com/in/emir-jean-antonios-698a82b7/"
+            />
+          </div>
+        </motion.div>
       </div>
       <div className="flex-1 flex justify-end">
-        <div className="relative  rounded-xl lg:w-[500px] lg:h-[500px]">
+        <motion.div className="relative  rounded-xl lg:w-[500px] lg:h-[500px]">
           <Image
             src={"/profile.jpeg"}
             layout="fill"
@@ -34,7 +49,7 @@ export default function Home() {
             objectFit="contain"
             className="rounded-xl grayscale"
           ></Image>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
