7f55911d5f7a76209229e123b5525946071110b3
Emir Jean Antonios <emir.antonios@gmail.com>
2023-09-14 12:28:46 +0300

    feature: added about me page.

---
+++ app/about-me/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+import Image from "next/image";
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
+
+export default function Home() {
+  return (
+    <section className="flex items-center h-full py-16">
+      <div className="basis-2/3">
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          className="text-7xl font-bold pb-8"
+        >
+          Hakkımda
+        </motion.h1>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.4 }}
+        >
+          <p className="text-xl leading-8 mb-4">
+            Merhaba, ben Emir Jean Antonios. Beykent üniversitesi Bilgisayar
+            Mühendisliği bölümünden mezunum. Bilgisayar mühendisliği serüvenime
+            Haydarpaşa Teknik ve Mesleki Anadolu Lisesinde Bilişim bölümü
+            okuyarak başladım. Lise yıllarımda PHP, C#, Javascript, HTML, CSS,
+            SQL dillerinin derslerini aldım ve orta seviye bir web developer
+            olarak Beykent üniversitesine başladım. Beykent Üniversitesinde
+            aldığım teknik dersler ile hem bir mühendis gibi düşünmeyi öğrendim
+            hem de yazılım bilgimi algoritmik düşünerek geliştirdim.
+          </p>
+          <p className="text-xl leading-8 mb-4">
+            Kanada'da 3 hafta kaldım. Kaldığım süre boyunca ingilizce eğitimi
+            aldım ve Kanada'yı gezme fırsatı yakaladım. Daha sonra Amerika'ya
+            geçtim ve Philadelphia, Washington ve New york'u gezdim. Lise 4 te
+            okulumun robot kulübü ile San francisco da 1 hafta boyunca robot
+            yarışmasında çevirmen olarak görev aldım. Üniversite yıllarımda
+            erasmus yaptım ve Çekya'da 6 ay kaldım.
+          </p>
+          <p className="text-xl leading-8 mb-4">
+            Şu an Amerika menşeli bir firmada remote Frontend Developer olarak
+            çalışıyorum. Bu deneyim, uluslararası bir perspektifle yazılım
+            geliştirme fırsatı sunuyor ve uzaktan çalışma becerilerimi
+            geliştirmeme yardımcı oluyor. Bu süreçte, farklı kültürlerden ve
+            uzmanlıklardan gelen ekip üyeleriyle işbirliği yaparak projelerde
+            daha etkili bir şekilde çalışmayı öğreniyorum. Ayrıca, modern web
+            teknolojileri ve uygulama geliştirme alanındaki son trendleri takip
+            ederek, projelerimizi her zaman en son teknolojilerle güncel
+            tutuyoruz. Bu deneyim, bilgisayar mühendisliği kariyerimi daha da
+            ileriye taşıma konusundaki tutkumu ve kararlılığımı pekiştiriyor.
+          </p>
+          <p className="text-xl leading-8">
+            Her gün yeni bir gelişme yaşadığımız bilgisayar teknolojisini takip
+            edip kendimi geliştiriyor ve çok sevdiğim mesleğimi en iyi şekilde
+            yapmaya çalışıyorum.
+          </p>
+        </motion.div>
+      </div>
+      <div className="flex-1 flex justify-end pl-4">
+        <motion.div className="relative  rounded-xl w-full lg:h-[500px]">
+          <Image
+            src={"/profile.jpeg"}
+            layout="fill"
+            alt="Emir Jean Antonios"
+            objectFit="cover"
+            className="rounded-xl grayscale"
+          ></Image>
+        </motion.div>
+      </div>
+    </section>
+  );
+}
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,19 +1,24 @@
+import Link from "next/link";
 import { FunctionComponent } from "react";
 
 interface HeaderProps {}
 
 const Header: FunctionComponent<HeaderProps> = () => {
-  return <header className="container mx-auto flex justify-between items-center">
-    <h2 className="text-xl font-bold">Emir Jean Antonios</h2>
-    <div>
+  return (
+    <header className="container mx-auto flex justify-between items-center">
+      <Link href={"/"}><h2 className="text-xl font-bold">Emir Jean Antonios</h2></Link>
+      <div>
         <ul className="flex gap-8">
-            <li>About Me</li>
-            <li>Skills</li>
-            <li>Projects</li>
-            <li>CV</li>
+          <li>
+            <Link href={"about-me"}>About Me</Link>
+          </li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>CV</li>
         </ul>
-    </div>
-  </header>;
+      </div>
+    </header>
+  );
 };
 
 export default Header;
